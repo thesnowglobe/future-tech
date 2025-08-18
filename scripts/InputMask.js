5f006534c16e615e123e1af370239c30a133dbe0
thesnowglobe <102154806+thesnowglobe@users.noreply.github.com>
2025-08-18 13:22:54 +0200

    feat: add & update scripts

---
+++ scripts/InputMask.js
@@ -0,0 +1,34 @@
+const rootSelector = '[data-js-input-mask]'
+
+class InputMask {
+  constructor(rootElement) {
+    this.rootElement = rootElement
+    this.init()
+  };
+
+  init() {
+    const isLibReady = typeof window.IMask !== 'undefined'
+
+    if (isLibReady) {
+      window.IMask(this.rootElement, {
+        mask: this.rootElement.dataset.jsInputMask
+      })
+    } else {
+      console.error('Library "imask" is not loaded!')
+    };
+  };
+};
+
+class InputMaskCollection {
+  constructor() {
+    this.init()
+  };
+
+  init() {
+    document.querySelectorAll(rootSelector).forEach((element) => {
+      new InputMask(element)
+    })
+  };
+};
+
+export default InputMaskCollection;
